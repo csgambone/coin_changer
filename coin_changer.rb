c788764b3cfa6c54f77a94817ec8c10d84fbd46c
Ciro Gambone <csgambone@gmail.com>
2013-04-29 00:43:32 -0500

    now with method

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,15 +1,22 @@
-result = []
 puts "Input change to be returned: "
-change = gets.chomp
-change = change.to_i
-result[0]=(change/100).floor
-change=change%100
-result[1]=(change/25).floor
-change=change%25
-result[2]=(change/10).floor
-change=change%10
-result[3]=(change/5).floor
-change=change%5
-result[4]=change
-puts "The correct change is #{result[0]} dollars, #{result[1]} quarters, #{result[2]} dimes, #{result[3]} nickels, and #{result[4]} pennies."
+
+def calculate(change)
+	#stuff
+	result = []
+	result[0]=(change/100).floor
+	change=change%100
+	result[1]=(change/25).floor
+	change=change%25
+	result[2]=(change/10).floor
+	change=change%10
+	result[3]=(change/5).floor
+	change=change%5
+	result[4]=change
+	return result
+end
+
+input = gets.chomp
+input = input.to_i
+output = calculate(input)
+puts "The correct change is #{output[0]} dollars, #{output[1]} quarters, #{output[2]} dimes, #{output[3]} nickels, and #{output[4]} pennies."
 
