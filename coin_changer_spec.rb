862d00edea96fcbd9694c3527720f2b480163873
Ciro Gambone <csgambone@gmail.com>
2013-04-29 20:25:27 -0500

    MOAR TESTS

--- coin_changer_spec.rb
+++ coin_changer_spec.rb
@@ -54,5 +54,35 @@ describe CoinChanger do
 		output = a.calculate(input)
 		output.should eq([1,0,0,0,0])
 	end
+
+
+
+	it "returns 0,0,0,1,1 when input is 6" do
+		input = 6
+		a = CoinChanger.new
+		output = a.calculate(input)
+		output.should eq([0,0,0,1,1])
+	end
+
+	it "returns 0,0,1,1,1 when input is 16" do
+		input = 16
+		a = CoinChanger.new
+		output = a.calculate(input)
+		output.should eq([0,0,1,1,1])
+	end
+
+	it "returns 0,1,1,1,1 when input is 41" do
+		input = 41
+		a = CoinChanger.new
+		output = a.calculate(input)
+		output.should eq([0,1,1,1,1])
+	end
+
+	it "returns 5,3,1,1,4 when input is 594" do
+		input = 594
+		a = CoinChanger.new
+		output = a.calculate(input)
+		output.should eq([5,3,1,1,4])
+	end
 end
 
